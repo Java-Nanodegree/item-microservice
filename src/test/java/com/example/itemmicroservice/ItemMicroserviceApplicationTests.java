c22de0b4ab9e5ccfb27c338e316c5e22efbbd45f
shasagar <shashaa35@gmail.com>
2019-09-26 01:18:47 +0530

    initial commit

---
+++ src/test/java/com/example/itemmicroservice/ItemMicroserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.itemmicroservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ItemMicroserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
